8b9b846cf377636b29ffecb2f5be556af4320c62
pskowronski97z <patryk.skowronski.97pl@gmail.com>
2025-08-21 18:10:56 +0200

    Initial version of CWindow class

---
+++ inc/CWindow.h
@@ -0,0 +1,37 @@
+#ifndef C_WINDOW_H_
+#define C_WINDOW_H_
+
+#include <Windows.h>
+
+#include <vector>
+#include <string>
+
+class CWindow {
+public:
+	class IEventListener {
+		friend class CWindow;
+	private:
+		virtual void OnClose() {};
+		virtual void OnCreate() {};
+		virtual void OnSizeChanged() {};
+		virtual void OnDestroy() {};
+		virtual void OnMoveEnter() {};
+		virtual void OnMoveExit() {};
+		virtual void OnPaint() {};
+	};
+
+	CWindow(const std::wstring title, const uint32_t width, const uint32_t height);
+	bool AddEventListener(IEventListener* pListener);
+	bool RemoveEventListener(IEventListener* pListener);
+	HWND GetHandle() const { return m_windowHandle; };
+
+private:
+	static LRESULT WindowProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+	HWND CreateSystemWindow(const std::wstring name, const uint32_t windowWidth, const uint32_t windowHeight);
+
+	HWND m_windowHandle = NULL;
+	static std::vector<std::pair<HWND, IEventListener*>> m_eventListeners;
+
+};
+
+#endif // !C_WINDOW_H_
---
+++ src/CWindow.cpp
@@ -0,0 +1,119 @@
+#include <CWindow.h>
+
+LRESULT CWindow::WindowProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
+
+	for (auto &listener : m_eventListeners) {
+		if (listener.first == hWnd && listener.second != nullptr) {
+			switch (Msg)
+			{
+			case WM_CREATE:
+				listener.second->OnCreate();
+				break;
+
+			case WM_PAINT:
+				listener.second->OnPaint();
+				break;
+
+			case WM_SIZING:
+				listener.second->OnSizeChanged();
+				break;
+
+			case WM_DESTROY:
+				listener.second->OnDestroy();
+				break;
+
+			case WM_ENTERSIZEMOVE:
+				listener.second->OnMoveEnter();
+				break;
+
+			case WM_EXITSIZEMOVE:
+				listener.second->OnMoveExit();
+				break;
+
+			case WM_CLOSE:
+				listener.second->OnClose();
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+	return DefWindowProc(hWnd, Msg, wParam, lParam);
+}
+
+HWND CWindow::CreateSystemWindow(const std::wstring name, const uint32_t windowWidth, const uint32_t windowHeight) {
+
+	HINSTANCE hInstance = GetModuleHandle(nullptr);
+	WNDCLASSEXW wndClassExW = { 0u };
+	const wchar_t windowClassName[] = L"DefaultWindowClass";
+
+	wndClassExW.lpszClassName = windowClassName;
+	wndClassExW.lpfnWndProc = WindowProc;
+	wndClassExW.hInstance = hInstance;
+	wndClassExW.hCursor = NULL;
+	wndClassExW.hIcon = NULL;
+	wndClassExW.hbrBackground = (HBRUSH)(COLOR_BTNTEXT);
+	wndClassExW.lpszMenuName = NULL;
+	wndClassExW.hIconSm = NULL;
+	wndClassExW.style = NULL;
+	wndClassExW.cbClsExtra = NULL;
+	wndClassExW.cbWndExtra = NULL;
+	wndClassExW.cbSize = sizeof(wndClassExW);
+
+	RegisterClassExW(&wndClassExW);
+
+	RECT clientArea = { 0 };
+	clientArea.top = 0u;
+	clientArea.left = 0u;
+	clientArea.bottom = windowHeight;
+	clientArea.right = windowWidth;
+
+	DWORD windowStyle = WS_SYSMENU | WS_MAXIMIZEBOX | WS_MINIMIZEBOX | WS_SIZEBOX;
+
+	AdjustWindowRectEx(&clientArea, windowStyle, TRUE, WS_EX_OVERLAPPEDWINDOW);
+
+	HWND systemWindowHandle = CreateWindowExW(
+		WS_EX_OVERLAPPEDWINDOW,
+		windowClassName,
+		name.c_str(),
+		windowStyle,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		clientArea.right - clientArea.left,
+		clientArea.bottom - clientArea.top + 5u,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	return systemWindowHandle;
+}
+
+CWindow::CWindow(const std::wstring title, const uint32_t width, const uint32_t height) 
+	: m_windowHandle(CreateSystemWindow(title, width, height))
+{
+
+}
+
+std::vector<std::pair<HWND, CWindow::IEventListener*>> CWindow::m_eventListeners;
+
+bool CWindow::AddEventListener(IEventListener* pListener) {
+	if (m_windowHandle && pListener) {
+		m_eventListeners.push_back({ m_windowHandle, pListener });
+		return true;
+	}
+	return false;
+};
+
+bool CWindow::RemoveEventListener(IEventListener* pListener) {
+	bool wasRemoved = false;
+	for (auto &listener : m_eventListeners) {
+		if (listener.second == pListener) {
+			listener.first = nullptr;
+			listener.second = nullptr;
+			wasRemoved = true;
+		}
+	}
+	return wasRemoved;
+}
